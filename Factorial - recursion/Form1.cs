71b6cb696ce2aabf0393d9eb83dd27fb4a613269
Steven Allison <Steven.Allison@riversidecollege.ac.uk>
2022-11-10 22:49:01 +0000

    Add project files.

---
+++ Factorial - recursion/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factorial___recursion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int x = int.Parse(textBox1.Text);
+                int y = factorial(x);
+                label1.Text = "factorial " + x + " = " + y;
+            }
+            catch
+            {
+                
+            }
+         }
+
+        int factorial (int x)
+        {
+            if (x == 0) return 1;
+            return (x * factorial(x-1));
+        }
+    }
+}
